327138682237de9a82ba0b2a0841b74329104ff8
Joshua Masseo <jmasseo@gmail.com>
2016-05-26 22:46:25 -0500

    Fixes, adding worker count

--- nats-http-agent.go
+++ nats-http-agent.go
@@ -43,6 +43,55 @@ func usage() {
 	log.Fatalf("Usage: nats-http-agent [-s server] [-t] <subject> <queue-group>\n")
 }
 
+type JobReq struct {
+	HTTPReq HTTPRequest
+	Reply   string
+}
+
+func workerTask(id int, jobs <-chan JobReq, nc *nats.Conn) {
+	i := 0
+	log.Printf("Loading worker %d", id)
+	for j := range jobs {
+		i++
+		myreq := j.HTTPReq
+		reply := j.Reply
+		req, err := http.NewRequest(myreq.Method, myreq.URL, strings.NewReader(myreq.Body))
+		for k, v := range myreq.Headers {
+			req.Header[k] = v
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Printf("[ERROR] [%d] Error Issuing HTTP Request[%v]: %+v", id, myreq.ID, err)
+			str, _ := json.Marshal(err)
+			nc.Publish("error", str)
+		}
+		bodystr, _ := ioutil.ReadAll(resp.Body)
+		myresp := &HTTPResponse{
+			ID:         myreq.ID,
+			WorkerName: workername,
+			Method:     myreq.Method,
+			URL:        myreq.URL,
+			Status:     resp.Status,
+			Body:       string(bodystr),
+			StatusCode: resp.StatusCode,
+			Headers:    resp.Header,
+		}
+		resp.Body.Close()
+
+		responseblock, _ := json.Marshal(myresp)
+		log.Printf("[INFO] [%d]  Replying to %+v with %+v", id, reply, string(responseblock))
+		err = nc.Publish(reply, responseblock)
+		if err != nil {
+			log.Printf("[ERROR] [%d] Replying Error [%v]: %+v", id, myreq.ID, err)
+			str, _ := json.Marshal(err)
+			nc.Publish("error", str)
+		}
+
+	}
+	log.Printf("Worker %d logged %d jobs.", id, i)
+}
+
+/*
 func printMsg(m *nats.Msg, i int, nconn *nats.Conn) {
 	log.Printf("[#%d] Received on [%s] Queue[%s] Pid[%d]: '%s'\n", i, m.Subject, m.Sub.Queue, os.Getpid(), string(m.Data))
 	myreq := HTTPRequest{}
@@ -52,6 +101,9 @@ func printMsg(m *nats.Msg, i int, nconn *nats.Conn) {
 		str, _ := json.Marshal(err)
 		nconn.Publish("error", str)
 	}
+	jobreq := JobReq{HTTPReq: myreq, Reply: m.Reply}
+	jobchan <- jobreq
+
 	req, err := http.NewRequest(myreq.Method, myreq.URL, strings.NewReader(myreq.Body))
 	for k, v := range myreq.Headers {
 		req.Header[k] = v
@@ -83,12 +135,12 @@ func printMsg(m *nats.Msg, i int, nconn *nats.Conn) {
 		nconn.Publish("error", str)
 	}
 }
-
+*/
 func main() {
 	client = &http.Client{}
 	var urls = flag.String("s", nats.DefaultURL, "The nats server URLs (separated by comma)")
 	var showTime = flag.Bool("t", false, "Display timestamps")
-
+	var workerCount = flag.Int("w", 1, "Worker Count")
 	log.SetFlags(0)
 	flag.Usage = usage
 	flag.Parse()
@@ -107,10 +159,25 @@ func main() {
 		log.Fatalf("Can't get system hostname: %v\n", err)
 	}
 	subj, queue, i := args[0], args[1], 0
-
-	nc.QueueSubscribe(subj, queue, func(msg *nats.Msg) {
+	jobchan := make(chan JobReq, 8096)
+	for w := 1; w <= *workerCount; w++ {
+		log.Printf("Launching worker %d", w)
+		go workerTask(w, jobchan, nc)
+	}
+	nc.QueueSubscribe(subj, queue, func(m *nats.Msg) {
 		i++
-		printMsg(msg, i, nc)
+		log.Printf("[#%d] Received on [%s] Queue[%s] Pid[%d]: '%s'\n", i, m.Subject, m.Sub.Queue, os.Getpid(), string(m.Data))
+		myreq := HTTPRequest{}
+		err := json.Unmarshal(m.Data, &myreq)
+		if err != nil {
+			log.Printf("[ERROR] Invalid HTTP Request: %+v", err)
+			str, _ := json.Marshal(err)
+			nc.Publish("error", str)
+		}
+		jobreq := JobReq{HTTPReq: myreq, Reply: m.Reply}
+		jobchan <- jobreq
+
+		//printMsg(msg, i, nc)
 	})
 
 	log.Printf("Listening on [%s]\n", subj)
